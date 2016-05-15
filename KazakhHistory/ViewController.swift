735a694d621ce3ca595d43c320c3729b63f704fd
Feanore <zhans.kenes@gmail.com>
2016-05-15 20:49:09 +0600

    Initial Commit

---
+++ KazakhHistory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KazakhHistory
+//
+//  Created by Zhanserik on 5/15/16.
+//  Copyright © 2016 Kenes Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
